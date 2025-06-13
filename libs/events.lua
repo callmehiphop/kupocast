2fbdf8c029fab6c42e895cb569fbded3b002d6b9
Dave Gramlich <callmehiphop@gmail.com>
2025-06-13 14:35:47 -0400

    fix(events): make off work for 1 time events

--- libs/events.lua
+++ libs/events.lua
@@ -8,33 +8,35 @@ function EventEmitter:new()
 end
 
 function EventEmitter:emit(event, ...)
-  local callbacks = self._events[event] or {}
-  for _, func in ipairs(callbacks) do
-    func(...)
+  local listeners = self._events[event] or {}
+  for _, listener in ipairs(listeners) do
+    listener.callback(...)
   end
 end
 
 function EventEmitter:on(event, callback)
-  local callbacks = self._events[event] or {}
-  table.insert(callbacks, callback)
-  self._events[event] = callbacks
+  local listeners = self._events[event] or {}
+  table.insert(listeners, { callback = callback })
+  self._events[event] = listeners
 end
 
 function EventEmitter:once(event, callback)
+  local listeners = self._events[event] or {}
   local function proxy(...)
-    self:off(event, proxy)
+    self:off(event, callback)
     callback(...)
   end
-  return self:on(event, proxy)
+  table.insert(listeners, { callback = proxy, original = callback })
+  self._events[event] = listeners
 end
 
--- TODO: support manual off() when bound with once()
 function EventEmitter:off(event, callback)
-  local callbacks = self._events[event] or {}
+  local listeners = self._events[event] or {}
   local filtered = {}
-  for _, func in ipairs(callbacks) do
+  for _, listener in ipairs(listeners) do
+    local func = listener.original or listener.callback
     if func ~= callback then
-      table.insert(filtered, func)
+      table.insert(filtered, listener)
     end
   end
   self._events[event] = (#filtered and filtered) or nil
