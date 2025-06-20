c5ccdb026aca9ed005b639c09fe577d4884471b7
Dave Gramlich <callmehiphop@gmail.com>
2025-06-19 22:53:08 -0400

    feat(events): pon/ponce to prepend listeners

--- libs/events.lua
+++ libs/events.lua
@@ -18,22 +18,39 @@ function EventEmitter:emit(event, ...)
   return true
 end
 
-function EventEmitter:on(event, callback)
+function EventEmitter:_on(event, prepend, listener)
   local listeners = self._events[event] or {}
-  table.insert(listeners, { callback = callback })
+  local index = (prepend and 1) or #listeners + 1
+  table.insert(listeners, index, listener)
   self._events[event] = listeners
   return self
 end
 
-function EventEmitter:once(event, callback)
-  local listeners = self._events[event] or {}
+function EventEmitter:_once(event, prepend, callback)
   local function proxy(...)
     self:off(event, callback)
     callback(...)
   end
-  table.insert(listeners, { callback = proxy, original = callback })
-  self._events[event] = listeners
-  return self
+  return self:_on(event, prepend, {
+    callback = proxy,
+    original = callback,
+  })
+end
+
+function EventEmitter:on(event, callback)
+  return self:_on(event, false, { callback = callback })
+end
+
+function EventEmitter:pon(event, callback)
+  return self:_on(event, true, { callback = callback })
+end
+
+function EventEmitter:once(event, callback)
+  return self:_once(event, false, callback)
+end
+
+function EventEmitter:ponce(event, callback)
+  return self:_once(event, true, callback)
 end
 
 function EventEmitter:off(event, callback)
