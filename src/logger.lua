2f524143724767a7e55be9a4753e5ed82089ff60
Dave Gramlich <callmehiphop@gmail.com>
2025-06-24 18:52:09 -0400

    feat(logger): time/timeEnd

--- src/logger.lua
+++ src/logger.lua
@@ -10,18 +10,44 @@ local Colors = {
   WARN = 104,
 }
 
-local function log(color, ...)
-  local messages = _.map({ ... }, function(message)
-    return (_.isTable(message) and dump(message)) or tostring(message)
-  end)
-  local output = _.join(messages, ' ')
-  print(chat.header('kupocast') .. chat.color1(color, output))
+local logger = {}
+local timers = {}
+
+local function serialize(message)
+  if _.isTable(message) then
+    return dump(message)
+  end
+  return tostring(message)
 end
 
-return {
-  info = _.bind(log, Colors.INFO),
-  error = _.bind(log, Colors.ERROR),
-  fatal = _.bind(log, Colors.FATAL),
-  success = _.bind(log, Colors.SUCCESS),
-  warn = _.bind(log, Colors.WARN),
-}
+local function log(color, ...)
+  local messages = _.map({ ... }, serialize)
+  local message = _.join(messages, ' ')
+  print(chat.header('kupocast') .. chat.color1(color, message))
+end
+
+logger.info = _.bind(log, Colors.INFO)
+logger.error = _.bind(log, Colors.ERROR)
+logger.fatal = _.bind(log, Colors.FATAL)
+logger.success = _.bind(log, Colors.SUCCESS)
+logger.warn = _.bind(log, Colors.WARN)
+
+function logger.time(name)
+  name = name or 'default'
+  if not _.isNil(timers[name]) then
+    return logger.warn("Timer '" .. name("' already exists"))
+  end
+  timers[name] = os.time()
+end
+
+function logger.timeEnd(name)
+  name = name or 'default'
+  if _.isNil(timers[name]) then
+    return logger.warn("Timer '" .. name .. "' does not exist")
+  end
+  local elapsed = os.difftime(os.time(), timers[name]) * 1000
+  timers[name] = nil
+  logger.info(name .. ':', tostring(elapsed), 'ms')
+end
+
+return logger
